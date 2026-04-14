d55d1a3d175fd75cd6bbd46aa62b8f0619dee43b
Oronte <orontesoler06@gmail.com>
2026-04-14 15:16:35 +0200

    commit

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Windows.Shell;
 
 namespace VisualStudioLauncher
 {
@@ -9,6 +10,29 @@ namespace VisualStudioLauncher
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            JumpList jumpList = new JumpList();
+
+            JumpTask task1 = new JumpTask
+            {
+                Title = "KrampusEngine",
+                Description = "Frappe atomique",
+            };
+
+            JumpTask task2 = new JumpTask
+            {
+                Title = "TrucDeMusiqueDeIoan",
+                Description = "Frappe pas trop atomique",
+            };
+
+            jumpList.JumpItems.Add(task1);
+            jumpList.JumpItems.Add(task2);
+
+            JumpList.SetJumpList(this, jumpList);
+        }
     }
 
 }
---
+++ ConfigureProjectPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VisualStudioLauncher
+{
+    /// <summary>
+    /// Interaction logic for ConfigureProjectPage.xaml
+    /// </summary>
+    public partial class ConfigureProjectPage : Page
+    {
+        public ConfigureProjectPage()
+        {
+            InitializeComponent();
+        }
+
+        void SearchProjectLocation(object sender, EventArgs e)
+        {
+            OpenFolderDialog _openFolder = new OpenFolderDialog();
+            bool? result = _openFolder.ShowDialog();
+
+            if (result == true)
+                location.Placeholder = _openFolder.FolderName;
+        }
+
+        void CreateProject(object sender, EventArgs e)
+        {
+            string _projectName = projectName.Placeholder;
+            string _location = location.Placeholder;
+        }
+    }
+}
