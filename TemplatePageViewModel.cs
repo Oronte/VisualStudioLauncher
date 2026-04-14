cbdf2a76ea49d6985eb3f3ef6c6414fc25789829
konishiba <yoan.gimenez1202@gmail.com>
2026-04-14 09:07:10 +0200

    viewTemplateBase

---
+++ TemplatePageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualStudioLauncher
+{
+    public class TemplatePageViewModel
+    {
+        ObservableCollection<Template> RecentsTemplate { get; set; } = new ObservableCollection<Template>();
+    }
+}
